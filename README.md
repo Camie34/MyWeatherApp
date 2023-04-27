f69effccd22c17381088bac0e944e80389093a5c
Mary <marymukami34@gmail.com>
2023-04-27 18:08:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MyWeatherApp
+
+This is a simple weather app that allows you to search for the current weather and 5-day forecast for any location. The app uses the OpenWeatherMap API to retrieve weather data.
+
+Getting Started
+To get started with the app, simply clone this repository and open the index.html file in your web browser. You can then enter a location in the search bar and click the "Search" button to retrieve weather data.
+
+API Key
+In order to use the OpenWeatherMap API, you will need to obtain an API key. You can do so by signing up for a free account at the following link: https://my-json-server.typicode.com/
+
+Once you have obtained your API key, you can add it to the app.js file by replacing the placeholder text with your actual API key:
+
+javascript
+Copy code
+const apiKey = "";
--- app.js
+++ app.js
@@ -1,69 +1,69 @@
-const form = document.querySelector('#location-form');
-const input = document.querySelector('#location-input');
-const loading = document.querySelector('#loading');
-const current = document.querySelector('#current');
-const temperature = document.querySelector('#temperature');
-const condition = document.querySelector('#condition');
-const icon = document.querySelector('#icon');
-const forecast = document.querySelector('#forecast');
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
+const API_KEY = '';
+
+const form = document.querySelector('form');
+const input = document.querySelector('#search');
+const loading = document.querySelector('#loading');
+const error = document.querySelector('#error');
+const weather = document.querySelector('#weather');
+const locationName = document.querySelector('#location');
+const currentTemperature = document.querySelector('#temperature');
+const currentCondition = document.querySelector('#condition');
+const currentIcon = document.querySelector('#icon');
+const forecastDays = document.querySelectorAll('.day-name');
+const forecastTemperatures = document.querySelectorAll('.day-temp');
+const forecastIcons = document.querySelectorAll('.day-icon');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
   const location = input.value;
-  getWeather(location);
+  await getWeather(location);
 });
 
 async function getWeather(location) {
-  loading.classList.remove('hidden');
-  current.classList.add('hidden');
-  forecast.innerHTML = '';
+  try {
+    loading.classList.remove('hidden');
+    error.classList.add('hidden');
+    weather.classList.add('hidden');
 
-  const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`);
-  const data = await res.json();
+    const response = await fetch(``);
+    const data = await response.json();
 
-  if (data.cod !== 200) {
+    if (data.cod !== 200) {
+      throw new Error(data.message);
+    }
+
+    const { name, main, weather } = data;
+    locationName.textContent = name;
+    currentTemperature.textContent = `${Math.round(main.temp)}°C`;
+    currentCondition.textContent = weather[0].description;
+    currentIcon.src = ``;
+
+    const forecastResponse = await fetch(``);
+    const forecastData = await forecastResponse.json();
+
+    if (forecastData.cod !== '200') {
+      throw new Error(forecastData.message);
+    }
+
+    const forecastDaysData = forecastData.list.filter((item) => item.dt_txt.includes('12:00:00'));
+    forecastDaysData.forEach((dayData, index) => {
+      const { dt_txt, main, weather } = dayData;
+      const date = new Date(dt_txt);
+      const dayName = date.toLocaleDateString('en-US', { weekday: 'long' });
+      const iconUrl = 
+      forecastDays[index].textContent = dayName;
+      forecastTemperatures[index].textContent = `${Math.round(main.temp)}°C`;
+      forecastIcons[index].src = iconUrl;
+    });
+
     loading.classList.add('hidden');
-    alert(data.message);
-    return;
-  }
-
-  const { main, weather } = data;
-  temperature.textContent = `${Math.round(main.temp)}°C`;
-  condition.textContent = weather[0].description;
-  icon.src = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
-
-  const forecastRes = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=metric`);
-  const forecastData = await forecastRes.json();
-
-  if (forecastData.cod !== '200') {
+    error.classList.add('hidden');
+    weather.classList.remove('hidden');
+  } catch (error) {
     loading.classList.add('hidden');
-    alert(forecastData.message);
-    return;
+    error.textContent = error.message;
+    error.classList.remove('hidden');
+    weather.classList.add('hidden');
   }
-
-  forecastData.list.slice(0, 5).forEach(forecast => {
-    const dayElem = document.createElement('div');
-    dayElem.classList.add('day');
-
-    const dayNameElem = document.createElement('div');
-    dayNameElem.classList.add('day-name');
-    dayNameElem.textContent = new Date(forecast.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' });
-
-    const dayTempElem = document.createElement('div');
-    dayTempElem.classList.add('day-temp');
-    dayTempElem.textContent = `${Math.round(forecast.main.temp)}°C`;
-
-    const dayIconElem = document.createElement('img');
-    dayIconElem.classList.add('day-icon');
-    dayIconElem.src = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
-
-    dayElem.appendChild(dayNameElem);
-    dayElem.appendChild(dayTempElem);
-    dayElem.appendChild(dayIconElem);
-
-    forecast.appendChild(dayElem);
-  });
-
-  loading.classList.add('hidden');
-  current.classList.remove('hidden');
 }
